c2dfb90ef2378c307091613a8cddbcc123bdd396
renee chan <reneechan122237@gmail.com>
2018-11-17 23:26:53 +0800

    getposts ok

---
+++ www/getjobpost.php
@@ -0,0 +1,19 @@
+<?php
+    try{
+    require_once('../conn.php');
+    $select = $pdo->query('select * from jobpost;');
+    // echo 'rows: '.$select->rowCount();
+    $jr = array();
+    if($select->rowCount() > 0){
+        foreach($select->fetchAll(PDO::FETCH_BOTH) as $row){
+            $temp = array('post_id'=>$row['id'], 'jobtitle'=>$row['jobtitle'],'experience'=>$row['experience'],'salary'=>$row['salary'],'skills'=>$row['skills'],'displayname'=>$row['displayname'],'shortdescription'=>$row['shortdescription'],'fielddesc'=>$row['fielddesc'],'subfielddesc'=>$row['subfielddesc'],'industrydesc'=>$row['industrydesc'],'minexp'=>$row['minexp'],'maxexp'=>$row['maxexp'],'date'=>$row['date'],'education'=>$row['education']);
+            array_push($jr, $temp);
+        }
+    }
+    $select->closeCursor();
+    echo json_encode($jr);
+    } catch(Exception $e){
+        die($e->getMessage());
+    }
+    
+?>
